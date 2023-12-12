65c7a6a252046651cdcd0bdbf030b80b982c12dc
shristi kamble <136729553+yesshristi@users.noreply.github.com>
2023-12-12 10:06:37 +0530

    Create CalculateWithOperators.java

---
+++ CalculateWithOperators.java
@@ -0,0 +1,17 @@
+public class CalculateWithOperators {
+    public static void main(String[] args){
+        int res = 1+2;
+        System.out.println(res);
+        res -= 1;
+        System.out.println(res);
+        res *= 2;
+        System.out.println(res);
+        res /= 2;
+        System.out.println(res);
+        res += 8;
+        System.out.println(res);
+        res %= 7;
+        System.out.println(res);
+    }
+    
+}
